07b61e08f4ac8d92051ed801e6792b040b28767b
Daniel <137360936+danielmrz-dev@users.noreply.github.com>
2024-12-02 20:59:08 -0300

    Upload das imagens pro Gist

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,30 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { AngularMaterialModule } from './components/angular-material/angular-material.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CardsContainerComponent } from './components/cards-container/cards-container.component';
+import { PerfumeCardComponent } from './components/perfume-card/perfume-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    CardsContainerComponent,
+    PerfumeCardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularMaterialModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [
-    provideClientHydration()
+    provideClientHydration(),
+    provideAnimationsAsync()
   ],
   bootstrap: [AppComponent]
 })
